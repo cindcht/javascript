18fe80a2bdc8ec4825b0170fc4e0fe225fe3881e
cindcht <cindy-dauchart@outlook.com>
2026-09-16 12:53:13 +0200

    correction - formulaire fonctionnel

--- js/script.js
+++ js/script.js
@@ -1,33 +1,26 @@
-// champs formulaire
-const form = document.querySelector('form');
+
+// Champs du formulaire
+
 const manga = document.querySelector('#manga');
 const genre = document.querySelector('#genre');
 const nbventes = document.querySelector('#nbventes');
-const photo = document.querySelector('#photo');
 
 
-//zones où afficher
+// Zones où afficher les résultats
 
 const affichermanga = document.querySelector('#affichermanga');
 const affichergenre = document.querySelector('#affichergenre');
 const affichernbventes = document.querySelector('#affichernbventes');
-const afficherphoto = document.querySelector('#afficherphoto');
-
-//valeurs par défauts
-
-afficherManga = "titre";
-affichergenre = "shonen";
-afffichenbventes = 0;
 
 
-
-//afficher
+// Fonction appelée lorsque l'on clique sur "Ajouter"
 
 function afficherCarte(event) {
+
     event.preventDefault();
-    affichermanga.texteContent = manga.value;
+
+    affichermanga.textContent = manga.value;
     affichergenre.textContent = genre.value;
     affichernbventes.textContent = nbventes.value;
-    afficherphoto.textContent = photo.value;
-}
 
+}
